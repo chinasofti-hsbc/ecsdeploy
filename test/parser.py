81ea190a5bd6e9741cf5b840a4cb49785bc039cf
jameson <miraclecome@gmail.com>
2017-10-26 17:16:09 +0800

    add parser

--- test/parser.py
+++ test/parser.py
@@ -3,6 +3,7 @@
 
 # Author: Liu Dan <miraclecome (at) gmail.com>
 
+import json
 from yaml import load, dump
 try:
 	from yaml import CLoader as Loader, CDumper as Dumper
@@ -12,7 +13,7 @@ except ImportError:
 def parser(fname):
 	with open(fname) as fd:
 		data = load(fd, Loader=Loader)
-		print(data)
+		print(json.dumps(data, indent=4, ensure_ascii=False))
 
 if __name__ == '__main__':
     parser('config.yml')
